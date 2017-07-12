7330ec8e1ba42c3abd618746623fd92253fb0db1
Gabriel Russell <TheBlargbla@gmail.com>
2017-07-11 23:06:53 -0400

    sample main.c for demonstration

---
+++ Class_Examples/Sample/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+  printf("Hello! This is a program. If created properly, this file should be labelled "app.out."\n");
+  
+  printf("To compile this again, type\n\n");
+  
+  printf("gcc main.c -o app.out");
+
+}
